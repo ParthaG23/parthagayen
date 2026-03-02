df3e1a58141759e1af7f6133e2002a9107183520
Partha Gayen <gayenpartha55@gmail.com>
2026-03-03 00:28:47 +0530

    add snakr project

--- src/data/Project.js
+++ src/data/Project.js
@@ -2,10 +2,22 @@ import cineverseImg from "../assets/projects/cineverse.webp";
 
 import tapasvImg from "../assets/projects/tapasvi.webp";
 import movienestImg from "../assets/projects/movienest.webp"
+import snakeGameImg from "../assets/projects/snakeGameImg.webp"
 
 const projects = [
   {
-    id: 1,
+  id: 4,
+  title: "Neon Snake Game",
+  description:
+    "A modern, mobile-optimized Snake game featuring smooth controls, neon UI effects, and performance-tuned gameplay built with React and Canvas.",
+  tech: ["React.js", "Tailwind CSS", "JavaScript", "Canvas API"],
+  github: "https://github.com/ParthaG23/sankeGame",
+  live: "https://sanke-game-kohl.vercel.app/",
+  image: snakeGameImg,
+  date: "Feb 14, 2026",
+},
+  {
+    id: 3,
     title: "CineVerse — Movie Information Platform",
     description:
       "Built a responsive movie information platform with categorized content and structured navigation. Focused on improving user experience through clean UI design and dynamic rendering.",
@@ -30,7 +42,7 @@ const projects = [
 
 
   {
-    id: 3,
+    id: 1,
     title: "Tapasvi — School Website",
     description:
       "Designed and developed a responsive school website featuring sections for admissions, facilities, and events, delivering a clean and user-friendly interface.",
